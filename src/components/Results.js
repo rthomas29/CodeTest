6449211c08939d3b5ff311fc33ba871cde2aa698
Rakeem <thomas.rakeem.29@gmail.com>
2018-02-07 09:15:12 -0500

    render radio chart with test results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Radar } from 'react-chartjs-2';
 
 const Results = props => {
+  const htmlTotal = props.results.HTML;
+  const cssTotal = props.results.CSS;
+  const jsTotal = props.results.JavaScript;
+  const data = {
+    labels: ['HTML', 'CSS', 'JavaScript'],
+    datasets: [
+      {
+        label: 'Points per category',
+        data: [htmlTotal, cssTotal, jsTotal],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+      },
+    ],
+  };
   return (
     <div>
       <h3>Results</h3>
@@ -15,6 +29,7 @@ const Results = props => {
         JavaScript: {props.results.JavaScript} of {props.totalTypeCount.JavaScript} -{' '}
         {props.calculateTotal(props.results.JavaScript, 2)}
       </p>
+      <Radar data={data} width={150} height={300} options={{ maintainAspectRatio: false }} />
     </div>
   );
 };
