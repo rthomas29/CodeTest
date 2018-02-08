704d8d809a9f6c944f0f3a110186ec4826966ac4
Rakeem <thomas.rakeem.29@gmail.com>
2018-02-08 11:47:53 -0500

    add modal to results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Doughnut } from 'react-chartjs-2';
-import { Button, Fade } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import '../stylesheets/Results.css';
 import 'chartjs-plugin-datalabels';
 
 class Results extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modal: false,
+    };
     this.htmlTotal = this.props.results.HTML;
     this.cssTotal = this.props.results.CSS;
     this.jsTotal = this.props.results.JavaScript;
@@ -39,9 +42,11 @@ class Results extends Component {
         },
       },
     };
+    this.handleToggle = this.handleToggle.bind(this);
   }
-  // keep the chart, remove the paragraphs.
-  // show total correct divided by total, then give more details about graph.
+  handleToggle() {
+    this.setState({ modal: !this.state.modal });
+  }
   render() {
     return (
       <div id="results-div" className="container d-flex flex-column justify-content-center align-items-center">
@@ -56,9 +61,30 @@ class Results extends Component {
               this.totalCorrect,
               this.props.questionCount,
             )} of the questions correctly.`}</p>
+            <Button color="primary" onClick={this.handleToggle}>
+              More Details
+            </Button>
           </aside>
           <div className="doughnut">
             <Doughnut data={this.data} width={150} height={150} options={this.options} />
+            <Modal isOpen={this.state.modal} toggle={this.handleToggle} className={this.props.className}>
+              <ModalHeader toggle={this.handleToggle}>Modal title</ModalHeader>
+              <ModalBody>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={this.handleToggle}>
+                  Do Something
+                </Button>{' '}
+                <Button color="secondary" onClick={this.handleToggle}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </Modal>
           </div>
         </div>
       </div>
