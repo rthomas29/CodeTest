f5b22ddd5ab5d3a145ba88245b93b88f27fda80f
Rakeem <thomas.rakeem.29@gmail.com>
2018-02-17 17:23:02 -0500

    Remove id prop from question schema. Will no longer need it due to MongoDB auto id property

--- server/react-backend/models/question.js
+++ server/react-backend/models/question.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
-  id: Number,
   question: String,
   type: String,
   answers: [String],
