5cd4cd7df754bab85b83241dc458c25ae8f84fbe
Rakeem <thomas.rakeem.29@gmail.com>
2018-02-17 16:52:46 -0500

    Started express integration and established pipeline for data from express to React frontend.

--- server/react-backend/routes/index.js
+++ server/react-backend/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api', function(req, res, next) {
+  res.send([{ name: 'Rakeem', age: 24 }, { name: 'someone', age: 24 }]);
 });
 
 module.exports = router;
