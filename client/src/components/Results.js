5802c5b9f0c3377b493f0f3039be4b023e4ae831
Rakeem <thomas.rakeem.29@gmail.com>
2018-03-19 19:11:10 -0400

    CLeaned up Results component by wrapping modal logic in ModalComponent

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Doughnut, HorizontalBar } from 'react-chartjs-2';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import ResultDetails from './ResultDetails';
+import GraphText from './GraphText';
+import ModalComponent from './ModalComponent';
 import '../stylesheets/Results.css';
 import 'chartjs-plugin-datalabels';
 
@@ -86,66 +87,24 @@ export default class Results extends Component {
   render() {
     return (
       <div id="results-div" className="container w-75 d-flex justify-content-center">
-        <div className="">
+        <div>
           <header className="header text-center">
             <h3 id="results-header" className="font-weight-light">
               How'd you do?
             </h3>
           </header>
           <div className="main-results d-flex flex-row">
-            <div className="graph">
-              <aside>
-                <p className="text-md-left lead">
-                  You answered {this.totalCorrect} out of {this.props.questionCount} questions correctly.<br />
-                </p>
-                <div className="btns" className="float-left">
-                  <Button id="button" color="primary" onClick={this.handleToggle}>
-                    More Details
-                  </Button>
-                  <Button color="danger" onClick={this.props.toggleLanding}>
-                    Try Again
-                  </Button>
-                </div>
-              </aside>
-            </div>
+            <GraphText
+              totalCorrect={this.totalCorrect}
+              questionCount={this.props.questionCount}
+              handleToggle={this.handleToggle}
+              toggleLanding={this.props.toggleLanding}
+            />
             <div className="doughnut">
               <Doughnut data={this.data} options={this.options} />
               <Modal isOpen={this.state.modal} toggle={this.handleToggle} className="small">
                 <ModalHeader toggle={this.handleToggle}>Breakdown</ModalHeader>
-                <ModalBody>
-                  <aside>Let's take a closer look at your results</aside>
-                  <HorizontalBar data={this.barData} options={this.barOptions} width={100} height={100} />
-                  <ResultDetails results={this.props.results} totalTypeCount={this.props.totalTypeCount} />
-                  <Button outline color="info" onClick={this.toggleNested}>
-                    Click for resources
-                  </Button>
-                  <Modal
-                    isOpen={this.state.nestedModal}
-                    toggle={this.toggleNested}
-                    onClosed={this.state.closeAll ? this.toggle : undefined}
-                  >
-                    <ModalHeader>Resources</ModalHeader>
-                    <ModalBody>
-                      <h3>Front End Web Development</h3>
-                      <ul>
-                        <li>
-                          <a href="https://developer.mozilla.org/en-US/docs/Learn">MDN - Learn Web Development</a>
-                        </li>
-                        <li>
-                          <a href="https://www.freecodecamp.org/">FreeCodeCamp - From 0 to Web Developer</a>
-                        </li>
-                        <li>
-                          <a href="https://javascript.info/">JavaScript.info - Modern JavaScript tutorial</a>
-                        </li>
-                      </ul>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button outline color="danger" onClick={this.toggleNested}>
-                        Close
-                      </Button>
-                    </ModalFooter>
-                  </Modal>
-                </ModalBody>
+                <ModalComponent barData={this.barData} barOptions={this.barOptions} results={this.props.results} totalTypeCount={this.props.totalTypeCount} toggleNested={this.toggleNested} nestedModal={this.state.nestedModal} closeAll={this.state.closeAll} toggle={this.handleToggle} />
                 <ModalFooter>
                   <Button outline color="primary" onClick={this.handleToggle}>
                     Close
